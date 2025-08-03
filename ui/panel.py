ab3264b6e30bc3d5741c706ad385c7a076bc950f
socrspy2 <originalxrayedits@gmail.com>
2025-08-03 04:01:29 +0200

    Update panel.py

--- ui/panel.py
+++ ui/panel.py
@@ -1,9 +1,8 @@
-# #############################################################################
-# --- UI (would be in 'ui/panel.py') ---
-# #############################################################################
+# --- File: ui/panel.py ---
 import bpy
 
-# --- ADDED: Import the operator classes so the panel can use their bl_idname ---
+# Import all the operator classes that the panel needs to reference.
+# The '..' means "go up one directory level" from ui/ to the addon root.
 from ..operators.view_navigator import VIEW_OT_set_view_axis
 from ..operators.sketch_tools import (
     SKETCH_OT_draw_line, SKETCH_OT_draw_rectangle, SKETCH_OT_draw_circle,
@@ -26,24 +25,15 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
     def _draw_ref_image_ui(self, layout, settings, view_name, view_type):
         """Helper function to draw the UI for a single reference image view."""
         image_settings = getattr(settings, f"{view_name.lower()}_image")
-
         box = layout.box()
         row = box.row()
-
-        # Show an icon if the image is loaded, otherwise use a placeholder
         icon = 'IMAGE_DATA' if image_settings.filepath else 'NONE'
         row.label(text=view_name, icon=icon)
-
-        # Show Clear button only if an image is loaded
         if image_settings.filepath:
             clear_op = row.operator(IMAGE_OT_clear_reference.bl_idname, text="Clear", icon='TRASH')
             clear_op.view_type = view_type
-
-        # Always show Load button
         load_op = row.operator(IMAGE_OT_load_reference.bl_idname, text="Load", icon='FILE_FOLDER')
         load_op.view_type = view_type
-
-        # If an image is loaded, show the controls for it
         if image_settings.filepath:
             col = box.column(align=True)
             col.use_property_split = True
@@ -52,7 +42,6 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
             col.prop(image_settings, "offset_x")
             col.prop(image_settings, "offset_y")
 
-
     def draw(self, context):
         layout = self.layout
         settings = context.scene.cad_tool_settings
@@ -60,20 +49,16 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
         # --- View Navigator Section ---
         view_box = layout.box()
         view_box.label(text="View Navigator", icon='VIEW3D')
-        
         row = view_box.row(align=True)
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Top").view_type = 'TOP'
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Front").view_type = 'FRONT'
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Right").view_type = 'RIGHT'
-        
         row = view_box.row(align=True)
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Bottom").view_type = 'BOTTOM'
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Back").view_type = 'BACK'
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Left").view_type = 'LEFT'
-
         row = view_box.row(align=True)
         row.operator(VIEW_OT_set_view_axis.bl_idname, text="Perspective", icon='VIEW_PERSPECTIVE').view_type = 'PERSP'
-        
         col = view_box.column(align=True)
         col.prop(settings, "pan_x", text="L/R")
         col.prop(settings, "pan_y", text="U/D")
@@ -82,8 +67,6 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
         ref_box = layout.box()
         ref_box.label(text="Reference Sketches", icon='IMAGE')
         ref_box.prop(settings, "show_ref_sketches", text="Show/Hide All", toggle=True)
-
-        # We can draw the UI for each view using our helper function
         self._draw_ref_image_ui(ref_box, settings, "Top", "TOP")
         self._draw_ref_image_ui(ref_box, settings, "Front", "FRONT")
         self._draw_ref_image_ui(ref_box, settings, "Right", "RIGHT")
@@ -91,37 +74,29 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
         self._draw_ref_image_ui(ref_box, settings, "Back", "BACK")
         self._draw_ref_image_ui(ref_box, settings, "Left", "LEFT")
 
-
         # --- Units & Grid Section ---
         unit_box = layout.box()
         unit_box.label(text="Units & Grid", icon='GRID')
-        
         row = unit_box.row(align=True)
         row.prop(settings, "unit_system", expand=True)
-        
         if settings.unit_system == 'METRIC':
             row = unit_box.row(align=True)
             row.prop(settings, "metric_unit", expand=True)
-        
         row = unit_box.row(align=True)
         row.prop(settings, "show_grid", text="Show Grid")
         row.prop(settings, "grid_spacing", text="Spacing")
 
-
         # --- 2D Sketching Section ---
         sketch_box = layout.box()
         sketch_box.label(text="2D Sketching", icon='GREASEPENCIL')
-        
         row = sketch_box.row(align=True)
         row.operator(SKETCH_OT_draw_line.bl_idname, text="Line", icon='CURVE_PATH')
         row.operator(SKETCH_OT_draw_rectangle.bl_idname, text="Rectangle", icon='MESH_PLANE')
         row.operator(SKETCH_OT_draw_circle.bl_idname, text="Circle", icon='MESH_CIRCLE')
-        
         row = sketch_box.row(align=True)
         row.operator(SKETCH_OT_draw_polyline.bl_idname, text="Poly-line", icon='MOD_MULTIRES')
         row.operator(SKETCH_OT_draw_arc.bl_idname, text="Arc", icon='CURVE_NCIRCLE')
         row.operator(SKETCH_OT_draw_circle_diameter.bl_idname, text="2P Circle", icon='MESH_CIRCLE')
-
         col = sketch_box.column(align=True)
         col.prop(settings, "use_grid_snap")
         col.prop(settings, "use_vertex_snap")
@@ -133,9 +108,7 @@ class VIEW3D_PT_cad_tools(bpy.types.Panel):
         op_box.operator(MESH_OT_inset_faces.bl_idname, text="Inset", icon='FACESEL')
         op_box.operator(MESH_OT_offset_edges.bl_idname, text="Offset", icon='EDGESEL')
         op_box.operator(MESH_OT_bevel_edges.bl_idname, text="Bevel", icon='MOD_BEVEL')
-
         op_box.separator()
-
         op_box.operator(MESH_OT_create_hole.bl_idname, text="Hole Tool", icon='MESH_CYLINDER')
         op_box.operator(MESH_OT_create_gear.bl_idname, text="Spur Gear", icon='MOD_ARRAY')
 
@@ -144,3 +117,10 @@ classes = (
     VIEW3D_PT_cad_tools,
 )
 
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
